34fb905de2cf3df1dc8a669fbc7f35b701699293
Dilan Madusanka <madusankadilan226@gmail.com>
2024-07-05 22:18:31 +0530

    user routes created

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import logo from './logo.svg';
+import './App.css';
+import Nav from './components/Nav';
+import Fotter from './components/Fotter';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Single from './pages/Single';
+import Home from './pages/Home';
+import Write from './pages/Write.js';
+ 
+
+
+
+const Layout =()=>{
+return(
+  <>
+  <Nav />
+  <Outlet />
+  <Fotter />
+
+  </>
+);
+
+}
+
+const router = createBrowserRouter ([
+
+
+  {
+    path:'/',
+    element:<Layout />,
+    children:[
+      {
+        path:'/',
+        element:<Home />,
+      },
+
+      {
+        path:'/post/:id',
+        element:<Single />,
+      },
+
+     {
+
+      path:'/write',
+      element:<Write />,
+     },
+    ]
+  },
+
+ {
+
+  path:'/register',
+  element : <Register />
+ },
+
+ {
+
+  path:'/login',
+  element: <Login />,
+
+ },
+
+]);
+
+
+
+function App() {
+  return (
+ <div>
+
+
+  <RouterProvider router={router} />
+ </div>
+
+  );
+}
+
+export default App;
---
+++ client/src/pages/Write.js
@@ -0,0 +1,9 @@
+
+
+function Write(){
+
+
+
+
+}
+export default Write;
