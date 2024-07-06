1a5af8b2e04e6f8c6bacae4fc182f86c097f482b
Dilan Madusanka <madusankadilan226@gmail.com>
2024-07-07 02:56:05 +0530

    backend routes

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Navigate, useNavigate } from 'react-router-dom'
+
+import axios from "axios";
 
 
 export default function Register() {
+
+  const [inputs , setInputs] = useState ({
+    username:"",
+    emial:"",
+    password:"",
+  })
+
+//we can input multiple input in one function
+ const handleChnage = e=>{
+
+    setInputs(prev=>({...prev, [e.target.name]:e.target.value}))
+
+ }
+
+
+ const handleSubmit= async (e)=>{
+
+   e.preventDefault();
+
+   try 
+   {
+    await axios.post("auth/register",inputs)
+    
+   } catch (err) {
+    console.log(err)
+    
+   }
+     
+ }
+
   return (
     <div>
 
@@ -10,12 +42,13 @@ export default function Register() {
      <div className="auth">
       <h1>Register</h1>
       <form action="">
-        <input type="text" placeholder='username' name='username'/>
-        <input type="email" placeholder='email' name='email'/>
+        <input type="text" placeholder='username' name='username' onChange={handleChnage}/>
+        <input type="email" placeholder='email' name='email' onChange={handleChnage} />
 
-        <input type="password" placeholder='password' name='password' />
+        <input type="password" placeholder='password' name='password'onChange={handleChnage} />
  
-        <button> Register</button>
+        <button onClick={handleSubmit}> Register</button>
+
         <p>invalid username or password</p>
         <span>Do have account ? <Link to ="/login">Login</Link> </span>
       </form>
