c314331f9d8d69135895c078228dc4c239faf254
Dilan Madusanka <madusankadilan226@gmail.com>
2024-07-06 13:34:39 +0530

    login and registration page created

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+
 
 export default function Login() {
   return (
     <div>
 
-  
+     
+     <div className="auth">
+      <h1>Login</h1>
+      <form action="">
+        <input type="text" placeholder='username' name='username'/>
+        <input type="password" placeholder='password' name='password' />
+
+        <button> Login</button>
+        <p>invalid username or password</p>
+        <span>Don't have account ? <Link to ="/register">Register</Link> </span>
+      </form>
+
+
+     </div>
 
 
  
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link, Navigate, useNavigate } from 'react-router-dom'
+
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div>
+
+      
+     <div className="auth">
+      <h1>Register</h1>
+      <form action="">
+        <input type="text" placeholder='username' name='username'/>
+        <input type="email" placeholder='email' name='email'/>
+
+        <input type="password" placeholder='password' name='password' />
+ 
+        <button> Register</button>
+        <p>invalid username or password</p>
+        <span>Do have account ? <Link to ="/login">Login</Link> </span>
+      </form>
+
+
+     </div>
+
+    </div>
   )
 }
